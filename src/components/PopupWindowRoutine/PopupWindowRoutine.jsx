34b16eceb2e8eea799000d830fddffddb1525590
Soufiane-coder <84711969+Soufiane-coder@users.noreply.github.com>
2023-07-24 19:13:46 +0100

    adding add routine popup page with its functionality

--- src/components/PopupWindowRoutine/PopupWindowRoutine.jsx
+++ src/components/PopupWindowRoutine/PopupWindowRoutine.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import { ReactComponent as Close } from '../../assets/icons/close.svg';
+import { ReactComponent as AddBoxIcon } from '../../assets/icons/add_box.svg';
 import $ from 'jquery';
 import { addRoutine } from "../../redux/routines/routines.actions";
 import { selectCurrentUser } from '../../redux/user/user.selector';
@@ -14,10 +15,27 @@ import data from '@emoji-mart/data'
 import Picker from '@emoji-mart/react';
 import { randomColor } from 'randomcolor';
 
+import TimeKeeper from "react-timekeeper";
+
 const PopupWindowRoutine = ({ user, addRoutine, popup, setPopup }) => {
     const [emoji, setEmoji] = useState("");
     const [showEmojiList, setShowEmojiList] = useState(false);
-    const [addRoutineForm, setAddRoutineForm] = useState({ title: "", description: "", important: false, level: 1, emoji: "" });
+    const [showTimePiker, setShowTimePicker] = useState(false);
+    const [addRoutineForm, setAddRoutineForm] = useState(
+        {
+            title: "",
+            description: "",
+            message: '',
+            priority: 'low',
+            level: 1,
+            emoji: "",
+            startRoutine: '12:00',
+            endRoutine: '12:00',
+        });
+
+    useEffect(() => {
+        console.log(addRoutineForm.startRoutine, addRoutineForm.endRoutine)
+    }, [addRoutineForm]);
 
     const handleChange = (event) => {
         const { name, value } = event.target;
@@ -29,7 +47,7 @@ const PopupWindowRoutine = ({ user, addRoutine, popup, setPopup }) => {
     }
     const handleSubmit = async (event) => {
         event.preventDefault();
-        if (!addRoutineForm.title && !addRoutineForm.description && !addRoutineForm.emoji) {
+        if (!addRoutineForm.title && !addRoutineForm.description) {
             alert("insert all fields");
             return;
         }
@@ -61,44 +79,123 @@ const PopupWindowRoutine = ({ user, addRoutine, popup, setPopup }) => {
         setShowEmojiList(false);
     }
 
+    const handleTimeRoutine = (event) => {
+        event.preventDefault();
+        const { name } = event.target;
+        setShowTimePicker(name);
+    }
+
+    const handleChangeTime = (newTime) => {
+        // console.log(newTime);
+        function formatTime(time) {
+            const [hours, minutes] = time.split(':');
+            const formattedTime = `${hours.padStart(2, '0')}:${minutes}`;
+            return formattedTime;
+        }
+
+        const timeRoutine = formatTime(newTime.formatted24);
+        console.log({ showTimePiker })
+        if (showTimePiker === "start-routine") {
+            setAddRoutineForm(old => ({ ...old, startRoutine: timeRoutine }))
+        } else if (showTimePiker === "end-routine") {
+            setAddRoutineForm(old => ({ ...old, endRoutine: timeRoutine }))
+        }
+    }
 
 
     return (
-        <div style={!popup ? { display: 'none' } : {}} className="popup" onClick={(event) => { if (event.target.closest(".choose-emoji") || event.target.closest("em-emoji-picker")) return; setShowEmojiList(false) }}>
+        <div style={!popup ? { display: 'none' } : {}} className="add-routine-window" >
             {
-                showEmojiList ? <Picker className="emoji-list" data={data} onEmojiSelect={handleEmoji} /> : ""
+                showEmojiList && <Picker className="emoji-list" data={data} onEmojiSelect={handleEmoji} />
+            }
+
+            {
+                showTimePiker && <div className="add-routine-window__clock">
+                    <TimeKeeper
+                        time={showTimePiker === "start-routine" ? addRoutineForm.startRoutine : addRoutineForm.endRoutine}
+                        onChange={handleChangeTime}
+                        doneButton={() => (
+                            <div
+                                className="add-routine-window__close-clock"
+                                onClick={() => setShowTimePicker(false)}
+                            >
+                                Close
+                            </div>
+                        )} />
+                </div>
             }
             <Zoom duration={500}>
-                < div className="popup__window" >
-                    <Close className="popup__window--close" onClick={(event) => {
-                        setPopup(false);
-                    }} />
-                    <div style={{ height: "80%" }}>
-                        <div className="popup__window--title">
-                            <span className="emoji" style={{ fontSize: "5rem" }}>{emoji}</span>
-                            <input placeholder="Title" type="text" onChange={handleChange} name="title" id="title" required />
+                <form className="add-routine-window__popup" onSubmit={handleSubmit}>
+                    <div className="add-routine-window__head">
+                        <AddBoxIcon className='add-routine-window__add-box-icon' />
+                        <h3 className="add-routine-window__title">
+                            Add routine
+                        </h3>
+                        <Close className="add-routine-window__close-icon" onClick={(event) => {
+                            setPopup(false);
+                        }} />
+                    </div>
+                    <h4 className='add-routine-window__title-input-label'>Title</h4>
+                    <input type="text" className="add-routine-window__title-input" name="title" id="routine-title"
+                        value={addRoutineForm.title}
+                        onChange={handleChange} />
+                    <h4 className='add-routine-window__description-input-label'>Description</h4>
+                    <textarea className='add-routine-window__description-input' name="description" id="routine-description" cols="30" rows="10"
+                        value={addRoutineForm.description}
+                        onChange={handleChange}></textarea>
+                    <h4 className='add-routine-window__message-input-label'>Message</h4>
+                    <textarea className='add-routine-window__message-input' name="message" id="routine-message" cols="30" rows="10"
+                        value={addRoutineForm.message}
+                        onChange={handleChange}></textarea>
+                    <div className="add-routine-window__hours-container">
+                        <div className="add-routine-window__start">
+                            <h4 className="add-routine-window__start-label">Start</h4>
+                            <input className="add-routine-window__start-time-input" type="time" name="start-routine"
+                                value={addRoutineForm.startRoutine}
+                                onClick={handleTimeRoutine}></input>
                         </div>
-                        <div className="popup__window--description">
-                            <textarea name="description" onChange={handleChange} placeholder="Description" id="description"></textarea>
+                        <div className="add-routine-window__end">
+                            <h4 className="add-routine-window__end-label">end</h4>
+                            <input className="add-routine-window__end-time-input" type="time" name="end-routine"
+                                value={addRoutineForm.endRoutine}
+                                onClick={handleTimeRoutine}
+                            ></input>
                         </div>
                     </div>
-                    <div style={{ height: "20%", display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-                        <div className="popup__window--level">
-                            <input type="number" onChange={handleChange} placeholder="level" name="level" id="level" min="1" max="5" value={addRoutineForm.level} />
+                    <div className="add-routine-window__footer-section">
+                        <div className="add-routine-window__diff-imp-container">
+                            <h4 className='add-routine-window__difficulty-input-label'>Difficulty</h4>
+                            <div class="add-routine-window__difficulty-input-div">
+                                <input className='add-routine-window__difficulty-input' type="range" name="level" id="range-difficulty" min="1" max="5"
+                                    value={addRoutineForm.level}
+                                    onChange={handleChange} />
+                                <p class="add-routine-window__difficulty-inputvalue">{addRoutineForm.level}🎚️</p>
+                            </div>
+                            <h4 className='add-routine-window__priority-input-label'>Priority</h4>
+                            <select className="add-routine-window__priority-input"
+                                name="priority"
+                                value={addRoutineForm.priority}
+                                onChange={handleChange}>
+                                <option className="add-routine-window__priority-option" value="apple">low</option>
+                                <option className="add-routine-window__priority-option" value="banana">medium</option>
+                                <option className="add-routine-window__priority-option" value="orange">important</option>
+                            </select>
                         </div>
-                        <div className="popup__window--important">
-                            <label className="container">
-                                <input type="checkbox" onChange={handleChange} id="important" />
-                                <div className="checkmark"></div>
-                                <label htmlFor="important">Important</label>
-                            </label>
-                        </div>
-                        <button className="choose-emoji" onClick={() => setShowEmojiList(true)}>choose emoji</button>
-                        <div className="popup__window--add-button">
-                            <input type="submit" onClick={handleSubmit} value="Add routine" />
+                        <div className="add-routine-window__emoji-add-btn-container">
+                            <div className="add-routine-window__emoji-section">
+                                <button className="add-routine-window__emoji-btn"
+                                    onClick={(event) => {
+                                        event.preventDefault();
+                                        setShowEmojiList(true)
+                                    }}>
+                                    Choose Emoji
+                                </button>
+                                <span className="add-routine-window__emoji-over-view">{emoji}</span>
+                            </div>
+                            <button className="add-routine-window__add-btn">Add Routine</button>
                         </div>
                     </div>
-                </div >
+                </form>
             </Zoom>
         </div >
 
